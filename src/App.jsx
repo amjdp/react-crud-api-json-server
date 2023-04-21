c1dcb6677da17f00fe810ee0ee9f80edefe28c80
amjdp <athiramj123@gmail.com>
2023-04-21 12:37:08 +0530

    home component added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Create from './components/Create';
 import Update from './components/Update';
 import Read from './components/Read';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(()=> {
+    axios.get('http://localhost:3000/users')
+    .then(res => setData(res.data))
+    .catch(err => console.log(err));
+  },[])
   return (
-    <div>Home</div>
+    <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
+        <h1>List of Users</h1>
+        <div className='w-75 rounded bg-white border shadow p-4 mt-5'>
+          <table className='table table-striped'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.map((d, i) => (
+                  <tr key={i}>
+                  <td>{d.id}</td>
+                  <td>{d.name}</td>
+                  <td>{d.email}</td>
+                  <td>{d.phone}</td>
+                  <td>
+                    <button className='btn btn-sm btn-info me-2'>Read</button>
+                    <button className='btn btn-sm btn-primary me-2'>Edit</button>
+                    <button className='btn btn-sm btn-danger'>Delete</button>
+                  </td>
+                </tr>
+                ))
+              }   
+            </tbody>
+          </table>
+        </div>
+    </div>
   )
 }
 
